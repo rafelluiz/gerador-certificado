7d0ae9ea68ba1d492abd4c6779e2c90944ec2765
Rafael Luiz <rafael.luiz@rebase.com.br>
2021-06-13 09:09:04 -0300

    Experimentando libs e testando salvamento
    * melhorando o html de template

---
+++ upload_express.js
@@ -0,0 +1,66 @@
+var express = require('express');
+const http = require('http'); // or 'https' for https:// URLs
+const fs = require('fs');
+const Axios = require('axios')
+const Handlebars = require("handlebars");
+const puppeteer = require('puppeteer');
+
+var app = express();
+
+app.use(express.json());
+
+app.post('/', function(request, response){
+  console.log(request.body.data.name);      // your JSON
+   // echo the result back
+  Axios({
+    method: "get",
+    url: request.body.file,
+    responseType: "stream"
+  }).then(function (response) {
+    response.data.pipe(fs.createWriteStream(`${__dirname}/uploads/cert.html`));
+  });
+  const source = fs.readFileSync(`${__dirname}/uploads/cert.html`, 'utf8')
+  var template = Handlebars.compile(source);
+
+  var data = { "name": request.body.data.name};
+  var result = template(data);
+
+  //fs.writeFile('cert_t.html','oi')
+
+  (async () => {
+    // launch a new chrome instance
+    const browser = await puppeteer.launch({
+      headless: true
+    })
+
+    // create a new page
+    const page = await browser.newPage()
+
+    // set your html as the pages content
+    ///const html = fs.readFileSync(`${__dirname}/template.html`, 'utf8')
+    await page.setContent(result, {
+      waitUntil: 'domcontentloaded'
+    })
+
+    // create a pdf buffer
+    const pdfBuffer = await page.pdf({
+      format: 'A4'
+    })
+
+    // or a .pdf file
+    await page.pdf({
+      format: 'A4',
+      path: `${__dirname}/my-fance-invoice.pdf`
+    })
+
+    // close the browser
+    await browser.close()
+  })()
+
+
+  response.send(result);
+  response.download(`${__dirname}/uploads/cert.html`)
+});
+
+
+app.listen(3000);
