5e17759b97e90abc7b970a25c480aa2988d42721
Rafael Luiz <rafael.luiz@rebase.com.br>
2021-06-14 07:53:16 -0300

    Ajustes no projeto
    * Remoção de bibliotecas desnecessárias
    * Definição de um Layoute para testes
    * Template de exemplo foi obtido na internet e carece de tradução
    
    Melhorias futuras:
    * Obter tags através do json no body
    
    Possíveis bugs a corrigir:
    * O projeto não valida url do template
    * Criar uma rotina para apagar arquivos da pasta upload
    * Refatorar o projeto

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-const fastify = require('fastify')({
-  logger:true
-})
+var express = require('express');
+const fs = require('fs');
+const Axios = require('axios')
+const Handlebars = require("handlebars");
+const puppeteer = require('puppeteer');
 
-fastify.post('/', function(request,reply) {
-  console.log(request.body)
-  let x = request.body
-  console.log('****')
-  console.log(x.template)
-  console.log(x.teste.oi)
-  console.log('****')
-  reply.send({x});
-})
+var app = express();
 
-fastify.listen(3000, function(err, address){
-  if (err){
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`server listening on ${address}`);
-})
+app.use(express.json());
+
+app.post('/', function(request, response){
+  console.log(request.body.data.name);      // your JSON
+  // echo the result back
+  let name_file = request.body.data.name;
+  let url = request.body.template
+  let source
+  Axios({
+    method: "get",
+    url: url,
+    responseType: "stream"
+  }).then(function (response_axios) {
+    response_axios.data.pipe(fs.createWriteStream(`${__dirname}/uploads/${name_file}.html`));
+  });
+
+
+  //console.log(source);
+  (async () => {
+    // launch a new chrome instance
+    const browser = await puppeteer.launch({
+      headless: true
+    })
+
+    // create a new page
+    const page = await browser.newPage()
+
+    source = fs.readFileSync(`${__dirname}/uploads/${name_file}.html`, 'utf8')
+
+    const template = Handlebars.compile(source);
+
+    const data = { "name": request.body.data.name, "template":request.body.template};
+    const result = template(data);
+
+    // set your html as the pages content
+    ///const html = fs.readFileSync(`${__dirname}/template.html`, 'utf8')
+    await page.setContent(result, {
+      waitUntil: 'domcontentloaded'
+    })
+
+    // create a pdf buffer
+    const pdfBuffer = await page.pdf({
+      format: 'A4'
+    })
+
+    // or a .pdf file
+    await page.pdf({
+      format: 'A4',
+      path: `${__dirname}/uploads/${name_file}.pdf`
+    })
+
+    // close the browser
+    await browser.close()
+    response.download(`${__dirname}/uploads/${name_file}.pdf`)
+
+  })()
+
+});
+
+app.listen(3000);
